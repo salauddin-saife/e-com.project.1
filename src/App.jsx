015391986c4fc428f8fd5178483ed84214911019
salauddin-saife <salauddin15saife@gmail.com>
2025-02-17 22:06:19 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<Root />}></Route>)
+);
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+};
 
-export default App
+export default App;
