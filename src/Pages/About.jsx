e83ceb195a2f26f23a7b80fd9a0cf49baf41bd47
salauddin-saife <salauddin15saife@gmail.com>
2025-02-19 05:56:14 +0600

    update

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Layout/Navbar'
 import Search_Tab from '../Layout/Search_Tab'
+import Footer from '../Layout/Footer'
 
 const About = () => {
   return (
     <div>
       <Navbar/>
       <Search_Tab />
+      <Footer/>
     </div>
   )
 }
